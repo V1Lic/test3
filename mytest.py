7779e7466a365ad3996b27b8133102d26996910a
Veljko Ilic <vilic@loyola.edu>
2021-09-16 13:16:48 -0400

    Initial commit

---
+++ mytest.py
@@ -0,0 +1,11 @@
+print(1+3)
+print(66)
+a = 5
+first = "drew"
+last = "mehr"
+print(a)
+print(first)
+b = 8
+print(a + b)
+print(first + ' '+ last)
+print("my age is", a, "and my name is", first + ' ' + last)
